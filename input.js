8a4cb1050dd4c7dc36d4278448c5e2d8900ff9a5
Kenny Lee <kenny.lee927@gmail.com>
2023-08-10 00:28:54 -0400

    mapped custom messages to keypresses

--- input.js
+++ input.js
@@ -9,6 +9,14 @@ const validInputs = {
   d: "Move: right",
 };
 
+const messageInputs = {
+  q: "Gnarly!",
+  e: "Nice!",
+  f: "Funny!",
+  g: "Good game!",
+  h: "Hisssssss",
+};
+
 // setup interface to handle user input from stdin
 const setupInput = function (conn) {
   // Store the connection object into top scoped variable
@@ -35,6 +43,11 @@ const handleUserInput = function (key) {
     if (key.toLowerCase() === input) {
       // send respective move command to connection object
       connection.write(validInputs[key.toLowerCase()]);
+    }
+  }
+  for (const input in messageInputs) {
+    if (key.toLowerCase() === input) {
+      connection.write(`Say: ${messageInputs[key.toLowerCase()]}`);
     }
   }
 };
