394e8ccef4ef03457d66c8c84fdca5578323f52b
Kenny Lee <kenny.lee927@gmail.com>
2023-08-09 23:54:07 -0400

    write and refactor keyboard input handling code

--- input.js
+++ input.js
@@ -1,5 +1,18 @@
+// Stores the active TCP connection object.
+let connection;
+
+// declare object with valid keypresses and their commands
+const validInputs = {
+  w: "Move: up",
+  a: "Move: left",
+  s: "Move: down",
+  d: "Move: right",
+};
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function (conn) {
+  // Store the connection object into top scoped variable
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -9,12 +22,21 @@ const setupInput = function() {
   return stdin;
 };
 
-const handleUserInput = function(key) {
-  console.log(key);
-  // \u0003 maps to ctrl+c input
+// callback function to run for every keypress
+const handleUserInput = function (key) {
+  // exit process if ctrl+c detected
   if (key === "\u0003") {
     process.exit();
   }
+  // loop through all valid keypresses
+  for (const input in validInputs) {
+    // toLowerCase to account for capslock
+    // check if keypress is a valid one
+    if (key.toLowerCase() === input) {
+      // send respective move command to connection object
+      connection.write(validInputs[key.toLowerCase()]);
+    }
+  }
 };
 
-module.exports = {setupInput};
+module.exports = { setupInput };
