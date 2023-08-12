745e0c6e82ee59e60e69dbd086f992e75406de00
Kenny Lee <kenny.lee927@gmail.com>
2023-08-12 19:19:02 -0400

    add and edit comments

--- client.js
+++ client.js
@@ -2,21 +2,25 @@ const net = require("net");
 const { IP, PORT, playerName, deathMessage } = require(`./constants`);
 
 const connect = function() {
+  // call createConnection method to create connection object based on net library
   const conn = net.createConnection({
     host: IP, // IP address here,
     port: PORT, // PORT number here,
   });
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  // on connection, show client success message and send player name to server
   conn.on("connect", () => {
     process.stdout.write("Successfully connected to game server\n");
     conn.write(`Name: ${playerName}`);
   });
-  // Death handling (idle or loss)
+  // death handling (idle or loss).
+  // show client death message and stop listening for inputs
   conn.on("data", () => {
     process.stdout.write(`${deathMessage}\n`);
     process.exit();
   });
+  // return connection object for manipulation elsewhere
   return conn;
 };
 
--- input.js
+++ input.js
@@ -1,4 +1,4 @@
-const {moveInputs, messageInputs} = require (`./constants`);
+const { moveInputs, messageInputs } = require(`./constants`);
 
 // Stores the active TCP connection object.
 let connection;
@@ -25,14 +25,18 @@ const handleUserInput = function (key) {
   // loop through all valid movement keypresses
   for (const input in moveInputs) {
     // toLowerCase to account for capslock
-    // check if keypress is a valid one
+    // check if keypress is a valid movement input
     if (key.toLowerCase() === input) {
       // send respective move command to connection object
       connection.write(`Move: ${moveInputs[key.toLowerCase()]}`);
     }
   }
+  // loop through all valid message keypresses
   for (const input in messageInputs) {
+    // toLowerCase to account for capslock
+    // check if keypress is a valid message input
     if (key.toLowerCase() === input) {
+      // issue 'Say:' command to server to send respective message
       connection.write(`Say: ${messageInputs[key.toLowerCase()]}`);
     }
   }
